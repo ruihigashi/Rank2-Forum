d38ed706a2c42ddf8ea35ccd883ddaee5c09dd60
rui0421 <rui.04215@icloud.com>
2025-10-02 15:42:05 +0900

    ポスト投稿時に、ポストリスト反映完成

--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -6,38 +6,41 @@ import Post from "./Post";
 
 export default function PostList() {
     // ポストリストコンテキスト、ユーザーコンテキストを使用する
-    const { postList, setPostList } = useContext(PostListContext);
+    const { postList } = useContext(PostListContext);
     const { userInfo } = useContext(UserContext);
+    const { setPostList } = useContext(PostListContext);
 
     // ポスト一覧を取得する関数
     const getPostList = async () => {
-        const posts = await
-         getList(userInfo.token);
+        console.log("MainLayout: getPostList");
+        const posts = await getList(userInfo.token);
         console.log(posts)
+
+        // getListで取得したポスト配列をコンテキストに保存する
+        let postList: Array<PostType> = [];
+        if (posts) {
+            posts.forEach((p: any) => {
+                postList.push({
+                    id: p.id,
+                    user_name: p.user_name,
+                    content: p.content,
+                    created_at: new Date(p.created_at),
+                });
+            });
+        }
+        setPostList(postList);
     }
+
+    // 描画時にポスト一覧を取得する
     useEffect(() => {
         getPostList();
     }, [])
-
-    // getListで取得したポスト配列をコンテキストに保存する
-    let postList: Array<PostType> = [];
-    if (posts) {
-        posts.forEach((p: any) => {
-            postList.push({
-                id: p.id,
-                user_name: p.user_name,
-                content: p.content,
-                created_at: new Date(p.created_at),
-            });
-        });
-    }
-    setPostList(postList);
 
     return (
         <div>
             <p>PostList</p>
             {postList.map((p) => (
-                <Post key={p.id} post={p}/>
+                <Post key={p.id} post={p} />
             ))}
         </div>
     )
--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,9 +1,32 @@
 import { useContext, useState } from "react"
 import { UserContext } from "../providers/UserProvider";
+import { PostListContext, PostType } from "../providers/PostListContext";
+import { getList } from "../api/Post";
 
 export default function SideBar() {
     const [msg, setMsg] = useState("");
     const { userInfo } = useContext(UserContext); // コンテキストからuserInfoを取り出す
+    const { setPostList } = useContext(PostListContext);
+
+    const getPostList = async () => {
+        const posts = await getList(userInfo.token);
+        console.log(posts);
+        let postList: Array<PostType> = [];
+        if (posts) {
+            console.log(posts);
+            posts.forEach((p: any) => {
+                postList.push({
+                    id: p.id,
+                    user_name: p.user_name,
+                    content: p.content,
+                    created_at: new Date(p.created_at),
+                });
+            });
+        }
+
+        setPostList(postList);
+
+    };
     const onSendClick = () => {
         console.log("onSendClick");
         console.log(userInfo); // 値の確認
