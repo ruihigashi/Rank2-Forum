39d1e3f7279504dfe2bca412bd25e0ad924b4fb2
rui0421 <rui.04215@icloud.com>
2025-10-08 16:36:40 +0900

    Header内のレイアウト完成

--- frontend/src/components/MainLayout.tsx
+++ frontend/src/components/MainLayout.tsx
@@ -7,7 +7,6 @@ import Contents from "./Contents";
 export default function MainLayout() {
     return (
         <>
-        // この部分のみstyled-components使用（復習のため）
             <SHeader>
                 <Header></Header>
             </SHeader>
@@ -24,15 +23,11 @@ export default function MainLayout() {
 }
 
 const SHeader = styled.div`
-    width: 100%;
-    height: 32px;
-    border: 2px solid red;
 `
 
 const SBody = styled.div`
 	width: 100%;
 	height: calc(100vh - 32px);
-	border: 2px solid green;
 	display: flex;
 	flex-direction: row
 `
@@ -44,7 +39,6 @@ const SSideBar = styled.div`
 `
 
 const SContents = styled.div`
-	border: 2px solid #FF00FF;;
 	width: 100%;
 	height: 100%;
 `
