de506f8abd65da18ce137b00f296a9a9eb5bc830
rui0421 <rui.04215@icloud.com>
2025-10-10 15:19:14 +0900

    モーダル内にユーザー情報を表示

--- frontend/src/components/UserDetailModal.tsx
+++ frontend/src/components/UserDetailModal.tsx
@@ -22,9 +22,23 @@ export default function UserDetailModal({ show, onClose, userDetails }: Props) {
                     </button>
                 </div>
                 <h3 className="text-xl font-semibold mb-4">ユーザー情報</h3>
-
+                {userDetails ? (
+                    <div className="text-gray-800">
+                        <p className="mb-2">
+                            <strong>ユーザー名:</strong> {userDetails.name}
+                        </p>
+                        <p className="mb-2">
+                            <strong>メールアドレス:</strong> {userDetails.umail || userDetails.email}
+                        </p>
+                        <p className="mb-2">
+                            <strong>作成日:</strong> {new Date(userDetails.created_at).toLocaleString()}
+                        </p>
+                    </div>
+                ) : (
+                    <p>読み込み中...</p>
+                )}
                 <div className="flex justify-center">
-                    <button className="text-white bg-blue-500 px-6 py-2 rounded">
+                    <button className="text-white bg-blue-500 mt-4 px-10 py-2 rounded">
                         編集
                     </button>
                 </div>
