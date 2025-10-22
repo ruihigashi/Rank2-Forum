cfe9b589d8bcc7b65ef111c37f9e0d34d497a080
rui0421 <rui.04215@icloud.com>
2025-10-22 15:41:56 +0900

    fix: .env内のバグ修正

--- backend/src/app.datasource.ts
+++ backend/src/app.datasource.ts
@@ -3,8 +3,6 @@ import * as dotenv from 'dotenv'
 
 dotenv.config();
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-
 const AppDataSource = new DataSource({
     type: "postgres", // 使用するデータベースの種類
     ...(process.env.DATABASE_URL
